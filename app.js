eff81eaad4a5cdbc645dd4a34d31c907c16e3bc7
EthanD <edanitz@zagmail.gonzaga.edu>
2024-10-09 08:50:43 -0700

    followed along in class

---
+++ app.js
@@ -0,0 +1,23 @@
+"use strict";
+
+//import modules
+import { addTask, clearInput } from "./todo.js";
+import { saveTasks, loadTasks } from "./storage.js";
+
+//import * as storage from "./storage.js"
+
+//set up DOM references
+const taskList = document.getElementById("task-list");
+const newTaskInput = document.getElementById("new-list");
+const addTaskButton = document.getElementById("add-list");
+
+//load tasks from local storage
+const tasks = loadTasks();
+
+//add event listener for new task
+addTaskButton.addEventListener("click", ()=>{
+    const task = newTaskInput.value.trim();
+    tasks.push(task);
+    saveTasks(tasks);
+    clearInput(newTaskInput);
+});
